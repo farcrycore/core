6c355dcdf5ae8dca4821cda388496f338ff19666
Matthew Bryant <mbryant@daemon.com.au>
2006-05-25 18:55:48 +0000

    Added datetimepicker and formValidation javascript libraries
    git-svn-id: https://farcry.jira.com/svn/farcry_core/trunk@97 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

---
+++ admin/js/formValidation/validation.js
@@ -0,0 +1,202 @@
+/*
+ * Really easy field validation with Prototype
+ * http://tetlaw.id.au/view/blog/really-easy-field-validation-with-prototype
+ * Andrew Tetlaw
+ * Version 1.4 (2006-05-18)
+ * Thanks:
+ *  Mike Rumble http://www.mikerumble.co.uk/ for onblur idea
+ *  Analgesia for spotting a typo
+ *  Paul Shannon http://www.paulshannon.com for the reset idea
+ *  Ted Wise for the focus-on-first-error idea
+ *  Sidney http://www.creativelycrazy.de/ for the custom advice idea
+ * 
+ * http://creativecommons.org/licenses/by-sa/2.5/
+ */
+Validator = Class.create();
+
+Validator.prototype = {
+	initialize : function(className, error, test) {
+		this.test = test ? test : function(){ return true };
+		this.error = error ? error : 'Validation failed.';
+		this.className = className;
+	}
+}
+
+var Validation = Class.create();
+
+Validation.prototype = {
+	initialize : function(form, options){
+		this.options = Object.extend({
+			stopOnFirst : false,
+			immediate : false,
+			focusOnError : true
+		}, options || {});
+		this.form = $(form);
+		Event.observe(this.form,'submit',this.onSubmit.bind(this),false);
+		if(this.options.immediate) {
+			Form.getElements(this.form).each(function(input) { // Thanks Mike!
+				Event.observe(input, 'blur', function(ev) { Validation.validate(Event.element(ev).id) });
+			});
+		}
+	},
+	onSubmit :  function(ev){
+		if(!this.validate()) {
+			Event.stop(ev);
+			if(this.options.focusOnError) {
+				$$('.validation-failed').first().focus();
+			}
+		}
+	},
+	validate : function() {
+		if(this.options.stopOnFirst) {
+			return Form.getElements(this.form).all(Validation.validate);
+		} else {
+			return Form.getElements(this.form).collect(Validation.validate).all();
+		}
+	},
+	reset : function() {
+		Form.getElements(this.form).each(Validation.reset);
+	}
+}
+
+Object.extend(Validation, {
+	validate : function(elm, index, options){ // index is here only because we use this function in Enumerations
+		var options = Object.extend({}, options || {}); // options still under development and here as a placeholder only
+		elm = $(elm);
+		var cn = elm.classNames();
+		return result = cn.all(Validation.test.bind(elm));
+	},
+	test : function(name) {
+		var v = Validation.get(name);
+		var prop = '__advice'+name.camelize();
+		if(Validation.isVisible(this) && !v.test($F(this))) {
+			if(!this[prop]) {
+				var advice = Validation.getAdvice(name, this.id);
+				if(typeof advice == 'undefined') {
+					advice = document.createElement('div');
+					advice.appendChild(document.createTextNode(v.error));
+					advice.className = 'validation-advice';
+					advice.id = 'advice-' + name + '-' + this.id;
+					advice.style.display = 'none';
+					this.parentNode.insertBefore(advice, this.nextSibling);
+				}
+				if(typeof Effect == 'undefined') {
+					advice.style.display = 'block';
+				} else {
+					new Effect.Appear(advice.id, {duration : 1 });
+				}
+			}
+			this[prop] = true;
+			this.removeClassName('validation-passed');
+			this.addClassName('validation-failed');
+			return false;
+		} else {
+			var advice = Validation.getAdvice(name, this.id);
+			if(typeof advice != 'undefined') advice.hide();
+			this[prop] = '';
+			this.removeClassName('validation-failed');
+			this.addClassName('validation-passed');
+			return true;
+		}
+	},
+	isVisible : function(elm) {
+		while(elm.tagName != 'BODY') {
+			if(!$(elm).visible()) return false;
+			elm = elm.parentNode;
+		}
+		return true;
+	},
+	getAdvice : function(name, id) {
+		var advice = Try.these(
+			function(){ return $('advice-' + name + '-' + id) },
+			function(){ return $('advice-' + id) }
+		);
+		return advice;
+	},
+	reset : function(elm) {
+		var cn = elm.classNames();
+		cn.each(function(value) {
+			var prop = '__advice'+value.camelize();
+			if(elm[prop]) {
+				var advice = Validation.getAdvice(value, elm.id);
+				advice.hide();
+				elm[prop] = '';
+			}
+			elm.removeClassName('validation-failed');
+			elm.removeClassName('validation-passed');
+		});
+	},
+	add : function(className, error, test, options) {
+		var nv = {};
+		nv[className] = new Validator(className, error, test, options);
+		Object.extend(Validation.methods, nv);
+	},
+	addAllThese : function(validators) {
+		var nv = {};
+		$A(validators).each(function(value) {
+				nv[value[0]] = new Validator(value[0], value[1], value[2], (value.length > 3 ? value[3] : {}));
+			});
+		Object.extend(Validation.methods, nv);
+	},
+	get : function(name) {
+		return  Validation.methods[name] ? Validation.methods[name] : new Validator();
+	},
+	methods : {}
+});
+
+//var $V = Validation.validate;
+//var $VG = Validation.get;
+//var $VA = Validation.add;
+
+Validation.add('IsEmpty', '', function(v) {
+				return  ((v == null) || (v.length == 0) || /^\s+$/.test(v));
+			});
+
+Validation.addAllThese([
+	['required', 'This is a required field.', function(v) {
+				return !Validation.get('IsEmpty').test(v);
+			}],
+	['validate-number', 'Please use numbers only in this field.', function(v) {
+				return Validation.get('IsEmpty').test(v) || !isNaN(v);
+			}],
+	['validate-digits', 'Please use numbers only in this field.', function(v) {
+				return Validation.get('IsEmpty').test(v) ||  !/[^\d]/.test(v);
+			}],
+	['validate-alpha', 'Please use letters only (a-z) in this field.', function (v) {
+				return Validation.get('IsEmpty').test(v) ||  /^[a-zA-Z]+$/.test(v)
+			}],
+	['validate-alphanum', 'Please use only letters (a-z) or numbers (0-9) only in this field. No spaces or other characters are allowed.', function(v) {
+				return Validation.get('IsEmpty').test(v) ||  !/\W/.test(v)
+			}],
+	['validate-date', 'Please enter a valid date.', function(v) {
+				var test = new Date(v);
+				return Validation.get('IsEmpty').test(v) || !isNaN(test);
+			}],
+	['validate-email', 'Please enter a valid email address. For example fred@domain.com .', function (v) {
+				return Validation.get('IsEmpty').test(v) || /\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/.test(v)
+			}],
+	['validate-date-au', 'Please use this date format: dd/mm/yyyy. For example 17/03/2006 for the 17th of March, 2006.', function(v) {
+				if(!Validation.get('IsEmpty').test(v)) {
+					var upper = 31;
+					if(/^(\d{2})\/(\d{2})\/(\d{4})$/.test(v)) { // dd/mm/yyyy
+						if(RegExp.$2 == '02') upper = 29;
+						if((RegExp.$1 <= upper) && (RegExp.$2 <= 12)) {
+							return true;
+						} else {
+							return false;
+						}
+					} else {
+						return false;
+					}
+				} else {
+					return true;
+				}
+			}],
+	['validate-currency-dollar', 'Please enter a valid $ amount. For example $100.00 .', function(v) {
+				// [$]1[##][,###]+[.##]
+				// [$]1###+[.##]
+				// [$]0.##
+				// [$].##
+				return Validation.get('IsEmpty').test(v) ||  /^\$?\-?([1-9]{1}[0-9]{0,2}(\,[0-9]{3})*(\.[0-9]{0,2})?|[1-9]{1}\d*(\.[0-9]{0,2})?|0(\.[0-9]{0,2})?|(\.[0-9]{1,2})?)$/.test(v)
+			}]
+]);
