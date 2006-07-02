acd1a97fdedc26afcff11ffbd18ddf7b054c0489
Matthew Bryant <mbryant@daemon.com.au>
2006-07-02 21:54:33 +0000

    Changes to prototype Tree Script
    git-svn-id: https://farcry.jira.com/svn/farcry_core/trunk@197 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

--- admin/js/prototypeTree/prototypeTree.js
+++ admin/js/prototypeTree/prototypeTree.js
@@ -1,5 +1,4 @@
 function toggletree(parentnode) {
-
 		Effect.toggle($(parentnode + '_wrap_content'),'slide', {duration:.3});
 
  		if (Element.visible($(parentnode + '_wrap_content')) == true) {
@@ -13,15 +12,14 @@ function toggletree(parentnode) {
 	function inittree(treeid) {
 		
 		var i = 0;			
-		var treelistitems = $$("#" + treeid + " table")	 
-
+		var treelistitems = $$("#" + treeid + " table")
 		treelistitems.each(function(treelistitem) {	
 			i++;
 			
 			var treelistitemiconlist = document.getElementsByClassName("nodeicon", $(treelistitem));
 			var treelistitemicons = $A(treelistitemiconlist);
 			treelistitemicons.each(function(treelistitemicon){
-
+				
 				Event.observe(treelistitemicon, 'click', function(event) {
 					//alert(treelistitem.id + '_content');
 					//var contentid = treelistitem.id + '_content';"
