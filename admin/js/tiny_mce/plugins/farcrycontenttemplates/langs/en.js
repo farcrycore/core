060ee0ee00333147f3ca8fe4ab8254e4369811b3
Matthew Bryant <mbryant@daemon.com.au>
2007-05-29 05:40:13 +0000

    Added language info for tinyMCE content template button.
    git-svn-id: https://farcry.jira.com/svn/FC/trunk@1568 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

--- admin/js/tiny_mce/plugins/farcrycontenttemplates/langs/en.js
+++ admin/js/tiny_mce/plugins/farcrycontenttemplates/langs/en.js
@@ -3,6 +3,6 @@
 /* Remember to namespace the language parameters lang_<your plugin>_<some name> */
 
 tinyMCE.addToLang('',{
-template_title : 'This is just a template popup',
-template_desc : 'This is just a template button'
+template_title : 'Related Object Content Templates',
+farcrycontenttemplates_desc : 'Related Object Content Templates'
 });
