8b5318c3831108c28eeb7bad9d056a004ca7cd7e
Matthew Bryant <mbryant@daemon.com.au>
2009-11-20 19:11:50 +0000

    FC-1439 Upgrade tinyMCE rich text editor to 3.2.7
    git-svn-id: https://farcry.jira.com/svn/FC/trunk@4903 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

---
+++ webtop/thirdparty/tiny_mce/plugins/fct/langs/en.js
@@ -0,0 +1,13 @@
+// UK lang variables
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+//tinyMCE.addToLang('',{
+//template_title : 'Related Object Content Templates',
+//farcrycontenttemplates_desc : 'Related Object Content Templates'
+//});
+
+tinyMCE.addI18n('en.fct',{
+	desc : 'Related Content Templates',
+	title: 'Related Content Templates'
+});
