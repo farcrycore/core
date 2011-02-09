farcrycore/core

commit fc60838743cfe3143a57a3a41aa503df8343c2fb

FC-2435 added application.url.publiclogin and application.url.webtoplogin
git-svn-id: https://farcry.jira.com/svn/FC/branches/p600@11524 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

tags/security/SelectUDLogin.cfm
@@ -12,7 +12,7 @@
 			<div class="fieldAlign">
 		</cfoutput>
 		
-				<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" onchange="window.location='#application.url.farcry#/login.cfm?ud='+this.value;"></cfoutput>
+				<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" onchange="window.location='#application.fapi.getLink(href=application.url.webtoplogin,urlParameters='ud='+this.value)#';"></cfoutput>
 				
 				<cfloop list="#application.security.getAllUD()#" index="thisud">
 					<cfoutput>

Application.cfc
@@ -750,6 +750,8 @@
 		<cfelse>
 			<cfset application.url.webtop = "#cgi.context_path##application.url.webroot#/webtop" />
 		</cfif>
+		<cfset application.url.webtoplogin = "#application.url.webtop#/login.cfm" />
+		<cfset application.url.publiclogin = application.url.webtoplogin />
 		<cfset application.url.farcry = "#application.url.webtop#" /><!--- Legacy variable. Developers should use application.url.webtop --->
 		<cfset application.url.imageRoot = "#application.url.webroot#">
 		<cfset application.url.fileRoot = "#application.url.webroot#/files">

packages/fourq/fourq.cfc
@@ -159,9 +159,8 @@ So in the case of a database called 'fourq' - the correct application.dbowner va
 			
 			<skin:loadJS id="jquery" />
 			
-			<skin:htmlHead id="webskinAjaxLoader">
+			<skin:loadJS id="webskinAjaxLoader">
 			<cfoutput>		
-				<script type="text/javascript">
 				$j.fn.loadAjaxWebskin = function (config){
 					var self = this;
 					config = config || self.data("loadWebskinAjax") || {};
@@ -192,9 +191,8 @@ So in the case of a database called 'fourq' - the correct application.dbowner va
 					
 					return self;
 				}
-				</script>
 			</cfoutput>
-			</skin:htmlHead>
+			</skin:loadJS>
 			
 			<!--- Get the url for the ajax webskin loader --->			
 			<!--- TODO: The ampDelim variable causes the link to be in the & 

tags/navajo/display.cfm
@@ -48,7 +48,7 @@
 <cfparam name="attributes.objectid" default="" />
 <cfparam name="attributes.typename" default="" />
 <!--- <cfparam name="attributes.method" default="" type="string" /> --->
-<cfparam name="attributes.loginpath" default="#application.url.farcry#/login.cfm?returnUrl=#URLEncodedFormat(cgi.script_name&'?'&cgi.query_string)#" type="string">
+<cfparam name="attributes.loginpath" default="#application.fapi.getLink(href=application.url.publiclogin,urlParameters='returnUrl='&URLEncodedFormat(cgi.script_name&'?'&cgi.query_string))#" type="string">
 
 <!--- passing in attributes.objectid will override the url value. This is done when a dmNavigation recusively calls the template. --->
 <cfif len(attributes.objectid)>
@@ -62,7 +62,6 @@
 <cfparam name="url.objectid" default="" />
 <cfparam name="url.type" default="" />
 <cfparam name="url.view" default="" />
-
 
 <!--- 
 <!--- Handle options for passing object/type in --->

tags/security/checkLoggedIn.cfm
@@ -72,8 +72,10 @@
 		
 		<cfif len(attributes.loginurl)>
 			<cfset loginURL = "#attributes.loginurl#" />
+		<cfelseif not findNoCase( "/webtop", cgi.script_name )>
+			<cfset loginURL = application.url.webtoplogin />
 		<cfelse>
-			<cfset loginURL = "#application.url.webtop#/login.cfm" />
+			<cfset loginURL = application.url.webtoplogin />
 		</cfif>
 		
 		

tags/security/selectProject.cfm
@@ -17,7 +17,7 @@
 					<div class="fieldSection string">
 						<label class="fieldsectionlabel" for="selectFarcryProject"> Project  : </label>
 						<div class="fieldAlign">
-							<select name="selectFarcryProject" id="selectFarcryProject" onchange="window.location='#application.url.webtop#/login.cfm?farcryProject='+this.value;">						
+							<select name="selectFarcryProject" id="selectFarcryProject" onchange="window.location='#application.fapi.getLink(href=application.url.webtoplogin,urlParameters='farcryProject='+this.value)#';">						
 								<cfloop from="1" to="#arraylen(aDomainProjects)#" index="i">
 									<cfif len(aDomainProjects[i])>
 										<option value="#aDomainProjects[i]#"<cfif cookie.currentFarcryProject eq aDomainProjects[i]> selected="selected"</cfif>>#server.stFarcryProjects[aDomainProjects[i]].displayname#</option>

webskin/dmNavigation/deniedaccess.cfm
@@ -1,5 +1,5 @@
 <!--- @@timeout: 0 --->
-<cfparam name="ARGUMENTS.STPARAM.loginpath" default="#application.url.farcry#/login.cfm?returnUrl=#URLEncodedFormat(cgi.script_name&'?'&cgi.query_string)#" type="string">
+<cfparam name="ARGUMENTS.STPARAM.loginpath" default="#application.fapi.getLink(href=application.url.webtoplogin,urlParameters='returnUrl='&URLEncodedFormat(cgi.script_name&'?'&cgi.query_string))#" type="string">
 <cfimport taglib="/farcry/core/tags/webskin" prefix="skin" />
 <skin:bubble title="Security" message="You do not have permission to access this part of the website" tags="security,warning" />
 <cflocation url="#ARGUMENTS.STPARAM.loginpath#&error=restricted" addtoken="No" />

webskin/farLogin/displayLogin.cfm
@@ -30,6 +30,12 @@ FARCRY IMPORT FILES
 
 
 
+<cfif structKeyExists(url,'returnurl') and len(trim(url.returnurl))>
+	<cfset stLocal.loginparams = 'returnurl='&urlEncodedFormat(url.returnurl) />
+<cfelse>
+	<cfset stLocal.loginparams = '' />
+</cfif>
+
 <!------------------ 
 START WEBSKIN
  ------------------>	
@@ -58,7 +64,7 @@ START WEBSKIN
 						<ft:fieldset>
 							<ft:field label="Project Selection" for="selectFarcryProject">
 								<cfoutput>
-								<select name="selectFarcryProject" id="selectFarcryProject" class="selectInput" onchange="window.location='#application.url.webtop#/login.cfm?<cfif structKeyExists(url,'returnurl') and len(trim(url.returnurl))>returnurl=#urlEncodedFormat(url.returnurl)#&</cfif>farcryProject='+this.value;">						
+								<select name="selectFarcryProject" id="selectFarcryProject" class="selectInput" onchange="window.location='#application.getLink(urlParameters=stLocal.loginparams)#&farcryProject='+this.value;">						
 									<cfloop from="1" to="#arraylen(aDomainProjects)#" index="i">
 										<cfif len(aDomainProjects[i])>
 											<option value="#aDomainProjects[i]#"<cfif cookie.currentFarcryProject eq aDomainProjects[i]> selected="selected"</cfif>>#server.stFarcryProjects[aDomainProjects[i]].displayname#</option>
@@ -67,7 +73,7 @@ START WEBSKIN
 								</select>
 								</cfoutput>
 							</ft:field>
-						</ft:fieldset>	
+						</ft:fieldset>
 					</cfif>
 				</cfif>			
 				
@@ -79,7 +85,7 @@ START WEBSKIN
 					<ft:fieldset>
 						<ft:field label="Select User Directory" for="selectuserdirectories">
 						
-							<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" class="selectInput" onchange="window.location='#application.url.farcry#/login.cfm?<cfif structKeyExists(url,'returnurl') and len(trim(url.returnurl))>returnurl=#urlEncodedFormat(url.returnurl)#&</cfif>ud='+this.value;"></cfoutput>
+							<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" class="selectInput" onchange="window.location='#application.getLink(urlParameters=stLocal.loginparams)#&ud='+this.value;"></cfoutput>
 							
 							<cfloop list="#application.security.getAllUD()#" index="thisud">
 								<cfoutput>

commit 0a91ad91881645da1080fc0d9930a3b3f791ef6c

fixed UD selection bug introduced in r11524 reverted javascript focus change as it was selecting the last error, not first, in r11558-11560
git-svn-id: https://farcry.jira.com/svn/FC/branches/p600@11564 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

tags/formtools/object.cfm
@@ -481,13 +481,6 @@
 						<!--- <div class="#request.stFarcryFormValidation[stObj.ObjectID][i].stError.class#">#request.stFarcryFormValidation[stObj.ObjectID][i].stError.message#</div> --->
 					</cfsavecontent>
 					
-					<skin:onReady id="focusValidationError"> <!--- grab prop name from app scope to ensure correct case --->
-						<cfoutput>
-							if (document.getElementById("#variables.prefix##application.stCoapi[stObj.typename].stProps[i].metadata.name#") != null)
-								document.getElementById("#variables.prefix##application.stCoapi[stObj.typename].stProps[i].metadata.name#").focus();
-						</cfoutput>
-					</skin:onReady>
-					
 					<cfset variables.errorClass = "error" />
 				</cfif>
 				

tags/security/SelectUDLogin.cfm
@@ -12,11 +12,11 @@
 			<div class="fieldAlign">
 		</cfoutput>
 		
-				<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" onchange="window.location='#application.fapi.getLink(href=application.url.webtoplogin,urlParameters='ud='+this.value)#';"></cfoutput>
+				<cfoutput><select name="selectuserdirectories" id="selectuserdirectories" onchange="window.location=this.value;"></cfoutput>
 				
 				<cfloop list="#application.security.getAllUD()#" index="thisud">
 					<cfoutput>
-						<option value="#thisud#"<cfif application.security.getDefaultUD() eq thisud> selected="selected"</cfif>>#application.security.userdirectories[thisud].title#</option>
+						<option value="#application.fapi.getLink(href=application.url.webtoplogin,urlParameters=thisud)#"<cfif application.security.getDefaultUD() eq thisud> selected="selected"</cfif>>#application.security.userdirectories[thisud].title#</option>
 					</cfoutput>
 				</cfloop>
 				