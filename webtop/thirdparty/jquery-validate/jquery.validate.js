7497e305feca883abd446af4ed1f85f219529925
Justin Carter <justin.w.carter@gmail.com>
2012-10-11 05:33:44 +0000

    FC-2699 jQuery validate plugin fix for "this[0] undefined" in delegate function
    git-svn-id: https://farcry.jira.com/svn/FC/trunk@12363 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

--- webtop/thirdparty/jquery-validate/jquery.validate.js
+++ webtop/thirdparty/jquery-validate/jquery.validate.js
@@ -311,9 +311,9 @@ $.extend($.validator, {
 			});
 
 			function delegate(event) {
-				var validator = $.data(this[0].form, "validator"),
+				var validator = $.data(this.form, "validator"),
 					eventType = "on" + event.type.replace(/^validate/, "");
-				validator.settings[eventType] && validator.settings[eventType].call(validator, this[0], event);
+				validator.settings[eventType] && validator.settings[eventType].call(validator, this, event);
 			}
 			$(this.currentForm)
 			       .delegate("[type='text'], [type='password'], [type='file'], select, textarea, " +
