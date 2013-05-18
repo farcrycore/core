farcrycore/core

commit 8979efdc2f843ec0895aafa74e848653c62ccdb0

FC-2784 added favourites menu
git-svn-id: https://farcry.jira.com/svn/FC/trunk@12604 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

tags/navajo/display.cfm
@@ -202,39 +202,44 @@
 	
 	<!--- determine display method for object --->
 	<cfset request.stObj = stObj>
+	<cfset url.type = stObj.typename />
 
 
 	<cfif len(url.view)>
-		<cftry>
+	
 		<!--- Update the view with the display method --->
 		<cfset url.view = application.fc.lib.device.getDeviceWebskin(stObj.typename, url.view) />
 		
-		<!--- Use the requested view --->
-		<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#url.view#" alternateHTML="" />
-
-		<cfcatch type="any">
-			<cfdump var="#cfcatch#">
-			<cfabort>
-		</cfcatch>
-		</cftry>
 	<cfelseif structKeyExists(stObj, "displayMethod") AND len(stObj.displayMethod)>
 		
 		<!--- Update the view with the display method --->
 		<cfset url.view = application.fc.lib.device.getDeviceWebskin(stObj.typename, stObj.displayMethod) />
 		
-		<!--- Use the display method stored with the object --->
-		<skin:view objectid="#stobj.objectid#" typename="#stobj.typename#" webskin="#url.view#" alternateHTML="" />
-
-	<cfelse>
-
+	<cfelseif application.fapi.hasWebskin(typename="#stobj.typename#", webskin="#stWebskins.page#")>
+		
 		<!--- All else fails, try the standard page webskin --->
-		<cfif application.fapi.hasWebskin(typename="#stobj.typename#", webskin="#stWebskins.page#")>
-			<skin:view objectid="#stobj.objectid#" typename="#stobj.typename#" webskin="#stWebskins.page#" />
-		<cfelse>
-			<cfset application.fc.lib.error.showErrorPage("404 Page missing",application.fc.lib.error.create404Error("I was looking at the type: #stobj.typename# and couldn't find a #stWebskins.page#. You'll want to create the default view for this object. To do that, create a #stWebskins.page#.cfm webskin in the webskin folder in a directory named #stobj.typename#.")) />
-		</cfif>
+		<cfset url.view = stWebskins.page />
+		
+	<cfelse>
+		
+		<cfset application.fc.lib.error.showErrorPage("404 Page missing",application.fc.lib.error.create404Error("I was looking at the type: #stobj.typename# and couldn't find a #stWebskins.page#. You'll want to create the default view for this object. To do that, create a #stWebskins.page#.cfm webskin in the webskin folder in a directory named #stobj.typename#.")) />
+		
 	</cfif>
-
+	
+	<!--- Use the requested view --->
+	<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#url.view#" alternateHTML="" r_html="result" />
+	
+	<cfif application.stCOAPI[stObj.typename].stWebskins[url.view].viewstack eq "data" and structkeyexists(application.stCOAPI[stObj.typename].stWebskins[url.view],"mimeType")>
+		<cfinvoke component="#application.fapi#" method="stream">
+			<cfinvokeargument name="content" value="#result#" />
+			<cfinvokeargument name="type" value="#application.stCOAPI[stObj.typename].stWebskins[url.view].mimeType#" />
+			<cfif structkeyexists(application.stCOAPI[stObj.typename].stWebskins[url.view],"filename")>
+				<cfinvokeargument name="filename" value="#application.stCOAPI[stObj.typename].stWebskins[url.view].filename#" />
+			</cfif>
+		</cfinvoke>
+	<cfelse>
+		<cfoutput>#result#</cfoutput>
+	</cfif>
 
 <cfelse>
 
@@ -282,7 +287,19 @@
 		<cfif application.fapi.hasWebskin(typename="#url.type#" , webskin="#url.view#")>
 
 			<!--- Call the view on the types coapi object --->
-			<skin:view typename="#url.type#" webskin="#url.view#" />
+			<skin:view typename="#url.type#" webskin="#url.view#" r_html="result" />
+			
+			<cfif application.stCOAPI[url.type].stWebskins[url.view].viewstack eq "data" and structkeyexists(application.stCOAPI[url.type].stWebskins[url.view],"mimeType")>
+				<cfinvoke component="#application.fapi#" method="stream">
+					<cfinvokeargument name="content" value="#result#" />
+					<cfinvokeargument name="type" value="#application.stCOAPI[url.type].stWebskins[url.view].mimeType#" />
+					<cfif structkeyexists(application.stCOAPI[url.type].stWebskins[url.view],"filename")>
+						<cfinvokeargument name="filename" value="#application.stCOAPI[url.type].stWebskins[url.view].filename#" />
+					</cfif>
+				</cfinvoke>
+			<cfelse>
+				<cfoutput>#result#</cfoutput>
+			</cfif>
 			
 		<cfelse>
 			

packages/lib/error.cfc
@@ -258,6 +258,63 @@
 				<cfreturn serializeJSON(arguments.exception) />
 			</cfcase>
 			
+			<cfcase value="xml">
+				<cfset output.append('<?xml version="1.0" encoding="UTF-8" ?><error>') />
+				<cfset output.append("<machineName><![CDATA[#xmlformat(arguments.exception.machineName)#]]></machineName>") />
+				<cfset output.append("<instancename><![CDATA[#xmlformat(arguments.exception.instancename)#]]></instancename>") />
+				<cfset output.append("<message><![CDATA[#xmlformat(arguments.exception.message)#]]></message>") />
+				<cfset output.append("<browser><![CDATA[#xmlformat(arguments.exception.browser)#]]></browser>") />
+				<cfset output.append("<datetime><![CDATA[#xmlformat(arguments.exception.datetime)#]]></datetime>") />
+				<cfset output.append("<host><![CDATA[#xmlformat(arguments.exception.host)#]]></host>") />
+				<cfset output.append("<httpreferer><![CDATA[#xmlformat(arguments.exception.httpreferer)#]]></httpreferer>") />
+				<cfset output.append("<querystring><![CDATA[#xmlformat(arguments.exception.querystring)#]]></querystring>") />
+				<cfset output.append("<remoteaddress>#xmlformat(arguments.exception.remoteaddress)#</remoteaddress>") />
+				<cfset output.append("<bot>#arguments.exception.bot#</bot>") />
+				<cfif structKeyExists(arguments.exception, "type") and len(arguments.exception.type)>
+					<cfset output.append("<type><![CDATA[#xmlformat(arguments.exception.type)#]]></type>") />
+				</cfif>
+				<cfif structKeyExists(arguments.exception, "detail") and len(arguments.exception.detail)>
+					<cfset output.append("<detail><![CDATA[#xmlformat(arguments.exception.detail)#]]></detail>") />
+				</cfif>
+				<cfif structKeyExists(arguments.exception, "extended_info") and len(arguments.exception.extended_info)>
+					<cfset output.append("<extended_info><![CDATA[#xmlformat(arguments.exception.extended_info)#]]></extended_info>") />
+				</cfif>
+				<cfif structKeyExists(arguments.exception, "queryError") and len(arguments.exception.queryError)>
+					<cfset output.append("<queryError><![CDATA[#xmlformat(arguments.exception.queryError)#]]></queryError>") />
+				</cfif>
+				<cfif structKeyExists(arguments.exception, "sql") and len(arguments.exception.sql)>
+					<cfset output.append("<sql><![CDATA[#xmlformat(arguments.exception.sql)#]]></sql>") />
+				</cfif>
+				<cfif structKeyExists(arguments.exception, "where") and len(arguments.exception.where)>
+					<cfset output.append("<where><![CDATA[#xmlformat(arguments.exception.where)#]]></where>") />
+				</cfif>
+				
+				<cfif structKeyExists(arguments.exception, "stack") and arraylen(arguments.exception.stack)>
+					<cfset output.append("<stack>") />
+					<cfloop from="1" to="#arrayLen(arguments.exception.stack)#" index="i">
+						<cfset output.append("<item>") />
+						<cfset output.append("<template><![CDATA[#xmlformat(arguments.exception.stack[i].template)#]]></template>") />
+						<cfset output.append("<line>#arguments.exception.stack[i].line#</line>") />
+						<cfset output.append("<location><![CDATA[#xmlformat(arguments.exception.stack[i].location)#]]></location>") />
+						<cfset output.append("</item>") />
+					</cfloop>
+					<cfset output.append("</stack>") />
+				</cfif>
+				
+				<cfif structKeyExists(arguments.exception, "url")>
+					<cfset output.append("<url>") />
+					<cfloop list="#listsort(structkeylist(arguments.exception.url),'textnocase')#" index="i">
+						<cfset output.append("<variable>") />
+						<cfset output.append("<name><![CDATA[#xmlformat(i)#]]></name>") />
+						<cfset output.append("<value><![CDATA[#xmlformat(arguments.exception.url[i])#]]></value>") />
+						<cfset output.append("</variable>") />
+					</cfloop>
+					<cfset output.append("</url>") />
+				</cfif>
+				
+				<cfset output.append("</error>") />
+			</cfcase>
+			
 			<cfcase value="html">
 				<cfset output.append("<h2>#padResource('error.overview@label','Error Overview')#</h2><table>") />
 				<cfset output.append("<tr><th>#padResource('error.overview.machine@label','Machine')#:</th><td>#arguments.exception.machineName#</td></tr>") />
@@ -449,6 +506,33 @@
 		</cfif>
 		
 		<cfparam name="application.url.webtop" default="/webtop">
+		
+		<!--- in the case of data views (json, xml, etc), return stream the data back in that type --->
+		<cfif isdefined("url.type") and isdefined("url.view") 
+			and isdefined("application.stCOAPI.#url.type#.stWebskins.#url.view#.viewStack") 
+			and application.stCOAPI[url.type].stWebskins[url.view].viewStack eq "data" 
+			and isdefined("application.stCOAPI.#url.type#.stWebskins.#url.view#.mimeType")>
+			
+			<cfswitch expression="#application.stCOAPI[url.type].stWebskins[url.view].mimeType#">
+				<cfcase value="json,text/json" delimiters=",">
+					<cfheader statuscode="#statuscode#" statustext="#statusmessage#" />
+					<cfif showError>
+						<cfcontent type="text/json" variable="#ToBinary( ToBase64( '{ "error":' & application.fc.lib.formatError(exception=arguments.stException,format='json') & '}' ) )#" reset="Yes" />
+					<cfelse>
+						<cfcontent type="text/json" variable="#ToBinary( ToBase64( '{ "error":"There was an error with that request" }' ) )#" reset="Yes" />
+					</cfif>
+				</cfcase>
+				<cfcase value="xml,text/xml" delimiters=",">
+					<cfheader statuscode="#statuscode#" statustext="#statusmessage#" />
+					<cfif showError>
+						<cfcontent type="text/xml" variable="#ToBinary( ToBase64( application.fc.lib.formatError(exception=arguments.stException,format='xml') ) )#" reset="Yes" />
+					<cfelse>
+						<cfcontent type="text/xml" variable="#ToBinary( ToBase64( '<?xml version="1.0" encoding="UTF-8" ?><error>There was an error with that request</error>' ) )#" reset="Yes" />
+					</cfif>
+				</cfcase>
+			</cfswitch>
+		</cfif>
+
 		
 		<cfcontent reset="true" />
 		<cfheader statuscode="#statuscode#" statustext="#statusmessage#" />

packages/lib/fapi.cfc
@@ -756,7 +756,55 @@
 
 		<cfreturn duplicate(result) />
 	</cffunction>
+	
+	<cffunction name="stream" access="public" output="false" returntype="void" hint="Stream content to the user with the specified mime type">
+		<cfargument name="content" type="string" required="true" />
+		<cfargument name="type" type="string" required="true" />
+		<cfargument name="filename" type="string" required="false" />
 		
+		<cfif structkeyexists(arguments,"filename") and len(arguments.filename)>
+			<cfheader name="Content-Disposition" value="attachment; filename=#arguments.filename#" />
+		</cfif>
+		
+		<cfswitch expression="#arguments.type#">
+			<cfcase value="html,htmlfragment" delimiters=",">
+				<cfset arguments.type = "text/html" />
+			</cfcase>
+			<cfcase value="text">
+				<cfset arguments.type = "text/html" />
+			</cfcase>
+			<cfcase value="json">
+				<cfset arguments.type = "text/json" />
+			</cfcase>
+			<cfcase value="xml">
+				<cfset arguments.type = "text/xml" />
+			</cfcase>
+			<cfcase value="csv">
+				<cfset arguments.type = "text/xml" />
+			</cfcase>
+		</cfswitch>
+		
+		<cfif not GetPageContext().GetResponse().IsCommitted()>
+			<cfimport taglib="/farcry/core/tags/misc" prefix="misc" />
+			
+			<cfif isdefined("request.fc.okToCache") and request.fc.okToCache>
+				<!--- Page ok to cache, a webskin has specified a cache timeout --->
+				<cfif not isdefined("request.fc.browserCacheTimeout") or request.fc.browserCacheTimeout eq -1>
+					<cfset request.fc.browserCacheTimeout = application.defaultBrowserCacheTimeout />
+				</cfif>
+				<cfif not isdefined("request.fc.proxyCacheTimeout") or request.fc.proxyCacheTimeout eq -1>
+					<cfset request.fc.proxyCacheTimeout = application.defaultProxyCacheTimeout />
+				</cfif>
+			<cfelse>
+				<cfset request.fc.browserCacheTimeout = 0 />
+				<cfset request.fc.proxyCacheTimeout = 0 />
+			</cfif>
+			
+			<misc:cacheControl browserSeconds="#request.fc.browserCacheTimeout#" proxySeconds="#request.fc.proxyCacheTimeout#" />
+		</cfif>
+		
+		<cfcontent type="#arguments.type#" variable="#ToBinary( ToBase64( trim(arguments.content) ) )#" reset="Yes" />
+	</cffunction>
 	
 	
 	<!--- @@examples:
@@ -943,7 +991,64 @@
 				<cfreturn structNew() />
 			</cfif>
 		</cffunction>
-	
+		
+		<!--- @@examples:
+			<p>Get the current user's favourites:</p>
+			<code>
+				<cfset aFavourites = application.fapi.getPersonalConfig("favourites",arraynew(1)) />
+			</code>
+		 --->
+		<cffunction name="getPersonalConfig" access="public" returntype="any" output="false" hint="Returns the personalisation value requested">
+			<cfargument name="key" type="string" required="true" />
+			<cfargument name="default" type="any" required="true" />
+			
+			<cfif not application.security.isLoggedIn()>
+				<cfreturn arguments.default />
+			</cfif>
+			
+			<cfif not structkeyexists(session.dmProfile,"wddxPersonalisation") or not len(session.dmProfile.wddxPersonalisation)>
+				<cfreturn arguments.default />
+			</cfif>
+			
+			<cfif not structkeyexists(session.dmProfile,"personalisation")>
+				<cfwddx action="wddx2cfml" input="#session.dmProfile.wddxPersonalisation#" output="session.dmProfile.personalisation" />
+			</cfif>
+			
+			<cfif structkeyexists(session.dmProfile.personalisation,arguments.key)>
+				<cfreturn session.dmProfile.personalisation[arguments.key] />
+			<cfelse>
+				<cfreturn arguments.default />
+			</cfif>
+		</cffunction>
+		
+		<!--- @@examples:
+			<p>Add a bookmark to the current user's favourites:</p>
+			<code>
+				<cfset aFavourites = application.fapi.getPersonalConfig("favourites",arraynew(1)) />
+				<cfset arrayappend(aFavourites,{ url:"http://twitter.com", label:"Twitter" }) />
+				<cfset application.fapi.setPersonalConfig("favourites",aFavourites) />
+			</code>
+		 --->
+		<cffunction name="setPersonalConfig" access="public" returntype="void" output="false" hint="Returns the personalisation value requested">
+			<cfargument name="key" type="string" required="true" />
+			<cfargument name="value" type="any" required="true" />
+			
+			<cfif not application.security.isLoggedIn()>
+				<cfreturn />
+			</cfif>
+			
+			<cfif not structkeyexists(session.dmProfile,"wddxPersonalisation") or not len(session.dmProfile.wddxPersonalisation)>
+				<cfset session.dmProfile.personalisation = structnew() />
+			</cfif>
+			
+			<cfset session.dmProfile.personalisation[arguments.key] = arguments.value />
+			
+			<cfwddx action="cfml2wddx" input="#session.dmProfile.personalisation#" output="session.dmProfile.wddxPersonalisation" />
+			
+			<cfset setData(stProperties=session.dmProfile) />
+		</cffunction>
+		
+		
 	<!--- GENERAL FARCRY --->
 	
 	

packages/lib/registerLibraries.cfc
@@ -114,6 +114,10 @@
 							baseHREF='#application.url.webtop#/thirdparty/bootstrap'
 							lFiles="bootstrap.min.js" />
 		
+		<skin:registerJS	id="webtop7" core="true"
+							baseHREF='#application.url.webtop#/js'
+							lFiles="webtop7.js" />
+		
 		<!--- CSS LIBRARIES --->
 		<skin:registerCSS 	id="webtop7"
 							baseHREF="#application.url.webtop#/css"

packages/types/dmProfile.cfc
@@ -35,7 +35,7 @@ TYPE PROPERTIES
 		ftType="image" ftDestination="/images/dmProfile/avatar"
 		ftAutoGenerateType="center" ftImageWidth="80" ftImageHeight="80"
 		ftAllowUpload="true" ftQuality="1.0" ftInterpolation="blackman">
-
+	
 	<cfproperty name="position" type="string" default="" required="no" hint="Profile object position" ftSeq="31" ftFieldSet="Organisation" ftLabel="Position" />
     <cfproperty name="department" type="string" default="" required="no" hint="Profile object department" ftSeq="32" ftFieldSet="Organisation" ftLabel="Department" />
 	
@@ -44,6 +44,8 @@ TYPE PROPERTIES
 	
 	<cfproperty name="notes" type="longchar" default="" required="no" hint="Additional notes" ftSeq="51" ftType="longchar" ftLabel="Notes" />
     
+	<cfproperty name="wddxPersonalisation" type="longchar" default="" required="no" hint="WDDX packet containing a user's personalisation settings." ftLabel="Personalization settings">
+	
 <!------------------------------
 OBJECT METHODS
 -------------------------------->

packages/types/farFU.cfc
@@ -738,6 +738,10 @@
 			
 			<cfset stResult["__allowredirect"] = stResult["__allowredirect"] and application.stCOAPI[stResult.type].stWebskins[stResult.view].allowredirect />
 			
+			<cfif application.stCOAPI[stResult.type].stWebskins[stResult.view].viewstack eq "data">
+				<cfset stResult["__allowredirect"] = false />
+			</cfif>
+			
 			<!--- Check the page viewbinding --->
 			<cfif structkeyexists(stResult,"objectid") and not listcontainsnocase("any,object",application.stCOAPI[stResult.type].stWebskins[stResult.view].viewbinding)>
 				<cfset stResult["404"] = "You are trying to bind an object [#stResult.objectid#] to a type webskin [#stResult.view#]" />
@@ -1009,7 +1013,7 @@
 									</cfif>
 								
 									<!--- We can call any webskin in the viewstack if in ajax mode --->
-									<cfif listcontainsnocase("page,any,ajax",stWS.viewstack) or findNoCase('ajaxmode',arguments.fuParameters)>
+									<cfif listcontainsnocase("page,any,data",stWS.viewstack) or findNoCase('ajaxmode',arguments.fuParameters)>
 										<cfset stResult.view = stWS.methodname />
 										<cfset fuVars = listdeleteat(fuVars,listfind(fuVars,"@pageview")) />
 	
@@ -1421,7 +1425,7 @@
 				<cfset viewFU = arguments.view />
 			</cfif>
 			<!--- If we have defined the view, and not the bodyView and the view is not set to page, then in order for the URL Parsing to work, we MUST explicitly tell the url that the webskin is for the view --->
-			<cfif not structKeyExists(stView, "viewStack") OR not listFind("page,any", stView.viewStack)>
+			<cfif not structKeyExists(stView, "viewStack") OR not listFind("page,any,data", stView.viewStack)>
 				<cfif not len(arguments.bodyView)>
 					<cfset bMustUseRegularURLParams = true />
 				</cfif>

webskin/UpdateApp/webtopBody.cfm
@@ -8,6 +8,8 @@
 	<ft:processformobjects typename="updateapp" />
 </ft:processform>
 
+<cfoutput><h1>Update Application</h1></cfoutput>
+
 <ft:form>	
 	<cfset qMetadata = application.forms['UpdateApp'].qMetadata />
 

webskin/dmProfile/ajaxAddFavourite.cfm
@@ -1,0 +1,56 @@
+<cfsetting enablecfoutputonly="true" />
+<!--- @@viewStack: data --->
+<!--- @@mimeType: json --->
+<!--- @@fuAlias: add-favourite --->
+
+<cfimport taglib="/farcry/core/tags/misc" prefix="misc" />
+
+<cfset aErrors = arraynew(1) />
+<cfif not structkeyexists(url,"favURL")>
+	<cfset arrayappend(aErrors,"Missing parameter [favURL]") />
+</cfif>
+<cfif not structkeyexists(url,"favLabel")>
+	<cfset arrayappend(aErrors,"Missing parameter [favLabel]") />
+</cfif>
+
+<cfif arraylen(aErrors)>
+	<cfoutput>{ "error" : #serializeJSON(aErrors)# }</cfoutput>
+<cfelse>
+	<cfset aFavourites = application.fapi.getPersonalConfig("favourites",arraynew(1)) />
+	
+	<!--- check to see if the page is already favourited --->
+	<cfset found = false />
+	<cfloop array="#aFavourites#" index="thisfavourite">
+		<cfif thisfavourite.url eq url.favURL>
+			<cfset found = true />
+		</cfif>
+	</cfloop>
+	
+	<cfif found>
+		<cfoutput>{ "error":["Page is already favourited"] }</cfoutput>
+	<cfelse>
+		<cfset arrayappend(aFavourites,{ url:url.favURL, label:favLabel }) />
+		<misc:sort values="#aFavourites#">
+			<cfif value1.label lt value2.label>
+				<cfset sendback = -1 />
+			<cfelseif value1.label eq value2.label>
+				<cfset sendback = 0 />
+			<cfelse>
+				<cfset sendback = 1 />
+			</cfif>
+		</misc:sort>
+		
+		<cfset application.fapi.setPersonalConfig("favourites",result) />
+		
+		<cfset position = 0 />
+		<cfloop from="1" to="#arraylen(aFavourites)#" index="i">
+			<cfif aFavourites[i].url eq url.favURL>
+				<cfset position = i />
+			</cfif>
+		</cfloop>
+		
+		<cfoutput>{ "success":true, "position":#position# }</cfoutput>
+	</cfif>
+</cfif>
+
+<cfsetting enablecfoutputonly="false" />

commit e929907eba15d453958ed351e2c6871362268174

fix nj:display bugs introduced in r12604
git-svn-id: https://farcry.jira.com/svn/FC/trunk@12642 8f7d33b6-1012-0410-9a5a-bb7ecce9c363

tags/navajo/display.cfm
@@ -202,44 +202,45 @@
 	
 	<!--- determine display method for object --->
 	<cfset request.stObj = stObj>
-	<cfset url.type = stObj.typename />
 
 
 	<cfif len(url.view)>
-	
+
 		<!--- Update the view with the display method --->
 		<cfset url.view = application.fc.lib.device.getDeviceWebskin(stObj.typename, url.view) />
-		
+		<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#url.view#" alternateHTML="" r_html="result" />
+
 	<cfelseif structKeyExists(stObj, "displayMethod") AND len(stObj.displayMethod)>
-		
+
 		<!--- Update the view with the display method --->
 		<cfset url.view = application.fc.lib.device.getDeviceWebskin(stObj.typename, stObj.displayMethod) />
-		
+		<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#url.view#" alternateHTML="" r_html="result" />
+
 	<cfelseif application.fapi.hasWebskin(typename="#stobj.typename#", webskin="#stWebskins.page#")>
-		
-		<!--- All else fails, try the standard page webskin --->
-		<cfset url.view = stWebskins.page />
-		
+
+		<!--- use the standard page webskin if there is one --->
+		<!--- NOTE: do not set url.view here as recursive calls from dmNavigation will break --->
+		<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#stWebskins.page#" alternateHTML="" r_html="result" />
+
 	<cfelse>
-		
+
 		<cfset application.fc.lib.error.showErrorPage("404 Page missing",application.fc.lib.error.create404Error("I was looking at the type: #stobj.typename# and couldn't find a #stWebskins.page#. You'll want to create the default view for this object. To do that, create a #stWebskins.page#.cfm webskin in the webskin folder in a directory named #stobj.typename#.")) />
 		
 	</cfif>
-	
-	<!--- Use the requested view --->
-	<skin:view objectid="#stobj.objectid#" typename="#stObj.typename#" webskin="#url.view#" alternateHTML="" r_html="result" />
-	
-	<cfif application.stCOAPI[stObj.typename].stWebskins[url.view].viewstack eq "data" and structkeyexists(application.stCOAPI[stObj.typename].stWebskins[url.view],"mimeType")>
+
+	<!--- either stream the webskin result with an appropriate mime type, or output it normally --->
+	<cfif application.stCOAPI[url.type].stWebskins[url.view].viewstack eq "data" and structkeyexists(application.stCOAPI[url.type].stWebskins[url.view],"mimeType")>
 		<cfinvoke component="#application.fapi#" method="stream">
 			<cfinvokeargument name="content" value="#result#" />
-			<cfinvokeargument name="type" value="#application.stCOAPI[stObj.typename].stWebskins[url.view].mimeType#" />
-			<cfif structkeyexists(application.stCOAPI[stObj.typename].stWebskins[url.view],"filename")>
-				<cfinvokeargument name="filename" value="#application.stCOAPI[stObj.typename].stWebskins[url.view].filename#" />
+			<cfinvokeargument name="type" value="#application.stCOAPI[url.type].stWebskins[url.view].mimeType#" />
+			<cfif structkeyexists(application.stCOAPI[url.type].stWebskins[url.view],"filename")>
+				<cfinvokeargument name="filename" value="#application.stCOAPI[url.type].stWebskins[url.view].filename#" />
 			</cfif>
 		</cfinvoke>
 	<cfelse>
 		<cfoutput>#result#</cfoutput>
 	</cfif>
+
 
 <cfelse>
 