178335223af207532b04685def87e7db624621ed
Justin Carter <justin.w.carter@gmail.com>
2014-01-04 21:39:15 +1100

    remove a whole lot more obsolete js from the webtop

--- webtop/includes/lib/qforms_full.js
+++
@@ -1,4 +0,0 @@
-// load the main qForms API
-document.write("<SCRIPT LANGUAGE=\"JavaScript\" SRC=\"/lib/qForms.js\"><\/SCRIPT>");
-// initialize all the custom methods
-document.write("<SCRIPT LANGUAGE=\"JavaScript\" SRC=\"/lib/qForms_init.js\"><\/SCRIPT>");
--- webtop/includes/lib/qforms_init.js
+++
@@ -1,8 +0,0 @@
-// set the path to the qForms directory
-qFormAPI.setLibraryPath("/lib/");
-// this loads all the default libraries
-qFormAPI.include("*");
-
-/********************************************************************
- load custom validation scripts here
-********************************************************************/
