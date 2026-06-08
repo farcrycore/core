94d84d5dbcdce8e4534407d57e048ed0d5ab4352
Justin Carter <justin.w.carter@gmail.com>
2026-06-08 21:42:27 +0800

    remove legacy codemirror plugin

--- webtop/thirdparty/tiny_mce/plugins/codemirror/langs/es_ES.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('es_ES',{
-	'HTML source code': 'Código fuente HTML',
-	'Start search': 'Buscar',
-	'Find next': 'Siguiente',
-	'Find previous': 'Anterior',
-	'Replace': 'Reemplazar',
-	'Replace all': 'Reemplazar todo'
-});
